348ba351b28a351fc37d248af7eb1a1677656ab1
Jesé Romero Arbelo <jeseromeroarbelo@gmail.com>
2021-06-07 16:42:54 +0100

    Auto-generated commit

--- assets/code-staters-data.ts
+++ assets/code-staters-data.ts
@@ -2,9 +2,9 @@ export default {
   'data': [
     {
       'username': 'Linkaynn',
-      'name': "Jesé Romero Arbelo",
+      'name': 'Jesé Romero Arbelo',
       'email': 'jeseromeroarbelo@gmail.com',
-      'personal_page': "http://www.jeseromero.com",
+      'personal_page': 'http://www.jeseromero.com',
       'description': 'Developing ideas since 2013 and on my road to be a Data Scientist. Angular Developer but also Python and Java lover, all languages have a place :)'
     },
     {
@@ -14,6 +14,12 @@ export default {
     {
       'username': 'BartLoch',
       'name': 'Sebastian Friedl'
+    },
+    {
+      'username': 'Nicd',
+      'name': 'Mikko Ahlroth',
+      'personal_page': 'https://blog.nytsoi.net/',
+      'description': 'Administrator of Code::Stats, double stack programmer from Finland.'
     }
   ]
 };
